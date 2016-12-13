bd068d65d650b602ee8fced80e5c79cf63b6a774
gennei <sai.gennei@gmail.com>
2016-12-14 00:14:41 +0900

    出力がANDであることをわかるよう修正

--- ch02/and_gate.py
+++ ch02/and_gate.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 import numpy as np
 
-
 def AND(x1, x2):
     x = np.array([x1, x2])
     w = np.array([0.5, 0.5])
@@ -15,4 +14,4 @@ def AND(x1, x2):
 if __name__ == '__main__':
     for xs in [(0, 0), (1, 0), (0, 1), (1, 1)]:
         y = AND(xs[0], xs[1])
-        print(str(xs) + " -> " + str(y))
+        print("AND" + str(xs) + " -> " + str(y))
