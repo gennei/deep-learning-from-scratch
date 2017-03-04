f98032b22e6bfdf4fbadacbd065352ca9aff2397
gennei <sai.gennei@gmail.com>
2017-03-04 18:15:45 +0900

    コメント追加

--- ch03/3_4_2.py
+++ ch03/3_4_2.py
@@ -10,7 +10,7 @@ def identity_function(x):
 
 def sotmax(a):
     c = np.max(a)
-    exp_a = np.exp(a - c)
+    exp_a = np.exp(a - c) # オーバーフロー対策
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
